1531e4d0f40be43e0194b6d0a53de1f6bb969436
Katie <costantinkp@gmail.com>
2022-03-01 14:56:09 -0500

    comments

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//creating schema
+//creates a new object called campsiteSchema
+//this is a constructor function (like classes without syntactic sugar)
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -13,8 +16,10 @@ const campsiteSchema = new Schema({
     }
 }, {
     timestamps: true
+    //will get a timestap when created and updated
 });
 
+//create model
 const Campsite = mongoose.model('Campsite', campsiteSchema);
-
+//need capitalized singular version of name of model we want to use- mongoose will look for a plural lowcase
 module.exports = Campsite;
